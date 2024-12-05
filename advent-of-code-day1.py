a2e9247c3ee2e844192a7ef1e30d7190b76f88c8
Lu Vy <luvy@umich.edu>
2024-12-04 20:48:58 -0500

    Add files via upload

---
+++ advent-of-code-day1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+A = []
+B = []
+
+with open('day1input.txt', 'r') as f:
+    for line in f:
+        num1, num2 = line.split()
+        A.append(int(num1))
+        B.append(int(num2))
+        
+# part a
+A.sort()
+B.sort()
+
+sum(abs(a-b) for (a,b) in zip(A,B))
+
+# part b
+from collections import Counter
+
+B_count = Counter(B)
+ans = 0
+
+for a in A:
+    if a in B_count:
+        ans = ans + a * B_count[a]
