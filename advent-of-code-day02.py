0cf48ce3e10039ff27974502b39480b4fa970457
Lu Vy <luvy@umich.edu>
2024-12-08 18:49:18 -0500

    Update advent-of-code-day02.py

--- advent-of-code-day02.py
+++ advent-of-code-day02.py
@@ -4,14 +4,11 @@ with open('day02input.txt', 'r') as f:
 # problem 1
 def isSafe(report):
     if report[0] <= report[-1]:
-        for i in range(len(report)-1):
-            if not (1 <= report[i+1] - report[i] <= 3):
-                return 0
-        return 1
-        
+        return all(1 <= report[i+1] - report[i] <= 3 
+                       for i in range(len(report)-1))
     else:
         return isSafe(report[::-1])
-
+        
 print(sum(isSafe(report) for report in reports))
 
 # problem 2
