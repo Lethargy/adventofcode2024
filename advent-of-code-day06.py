22827afcd4886181e8f5d05997f4bd15afe35edb
Lu Vy <luvy@umich.edu>
2024-12-11 12:01:57 -0500

    Update advent-of-code-day06.py

--- advent-of-code-day06.py
+++ advent-of-code-day06.py
@@ -6,7 +6,7 @@ with open('day06input.txt', 'r') as file:
 n = len(room)
 m = len(room[0])
 
-def next_step(i,j,d):
+def next_step(i: int, j: int, d: str) -> tuple[int]:
     if d == '^':
         return i-1,j
     if d == '>':
@@ -43,24 +43,24 @@ print(len(visited))
 
 # part 2
 
-def isLoop(i_obs,j_obs):
+def isLoop(i_obs: int, j_obs: int) -> bool:
     i0 = i00; j0 = j00; d = '^'; k = 0
     visited  = {(i0,j0,d)}
     i1,j1 = next_step(i0,j0,d)
     
-    while (0 <= i1 <= n-1) and (0 <= j1 <= n-1):
+    while (0 <= i1 <= n-1) and (0 <= j1 <= m-1):
         if room[i1][j1] == '#' or (i1,j1) == (i_obs,j_obs):
             k = k + 1
             d = directions[k % 4]
         else:
             if (i1,j1,d) in visited:
-                return 1
+                return True
 
             visited.add((i1,j1,d))
             i0,j0 = i1,j1
 
         i1,j1 = next_step(i0,j0,d)
         
-    return 0
+    return False
 
 print(sum(isLoop(a,b) for (a,b) in visited - {(i00,j00)}))
