7007f34a3c3120491937a40d26a5aa4ecb60bdc0
Lu Vy <luvy@umich.edu>
2024-12-11 18:38:27 -0500

    Update advent-of-code-day02.py

--- advent-of-code-day02.py
+++ advent-of-code-day02.py
@@ -2,7 +2,7 @@ with open('day02input.txt', 'r') as f:
     reports = [[int(x) for x in line.split()] for line in f]
         
 # problem 1
-def isSafe(report):
+def isSafe(report: list[int]) -> bool:
     if report[0] <= report[-1]:
         return all(1 <= report[i+1] - report[i] <= 3 
                        for i in range(len(report)-1))
@@ -12,7 +12,7 @@ def isSafe(report):
 print(sum(isSafe(report) for report in reports))
 
 # problem 2
-def canBeSafe(report):
+def canBeSafe(report: list[int]) -> bool:
     for i in range(len(report)):
         if isSafe(report[:i] + report[i+1:]):
             return 1
